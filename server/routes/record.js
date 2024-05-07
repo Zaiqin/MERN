5ca53608c9ddb38e357fd9e7f8b01d44d71a1df1
Zaiqin <phuazaiqin@gmail.com>
2024-05-08 00:10:50 +0800

    SERVER

--- server/routes/record.js
+++ server/routes/record.js
@@ -13,32 +13,20 @@ const router = express.Router();
 
 // This section will help you get a list of all the records.
 router.get("/", async (req, res) => {
-  console.log("a")
   let collection = await db.collection("records");
   let results = await collection.find({}).toArray();
-  console.log("asdf")
   res.send(results).status(200);
 });
 
 // This section will help you get a single record by id
 router.get("/:id", async (req, res) => {
-  console.log("Requesting record with ID:", req.params.id);
-  try {
-    let collection = await db.collection("records");
-    let query = { _id: new ObjectId(req.params.id) };
-    let result = await collection.findOne(query);
-    if (!result) {
-      console.log("Record not found");
-      return res.status(404).send("Not found");
-    }
-    console.log("Record found:", result);
-    res.status(200).send(result);
-  } catch (error) {
-    console.error("Error fetching record:", error);
-    res.status(500).send("Error fetching record");
-  }
-});
+  let collection = await db.collection("records");
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.findOne(query);
 
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
 
 // This section will help you create a new record.
 router.post("/", async (req, res) => {
