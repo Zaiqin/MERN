28d70e8c38796bd9be6f4e9bc87ba48176949b11
Zaiqin <phuazaiqin@gmail.com>
2024-05-07 16:43:57 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ import cors from "cors";
 import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5050;
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5050"; // Change the default value to match your development environment
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/record", records);
+app.use(`${API_BASE_URL}/record`, records);
 
 // start the Express server
 app.listen(PORT, () => {
