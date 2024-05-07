efb2b8da44de6fed2f03564af13c680957f00bc2
Zaiqin <phuazaiqin@gmail.com>
2024-05-07 17:32:40 +0800

    asdf

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
 
+const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/record", records); // Changed from "/record" to "/api/record" for consistency
+app.use("/record", records);
 
-// Export the Express app as a serverless function
-export default app;
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
