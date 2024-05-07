414e2b70ddff1727457ebdeb19662c29e61d5b72
Zaiqin <phuazaiqin@gmail.com>
2024-05-07 23:53:24 +0800

    Update record.js

--- server/routes/record.js
+++ server/routes/record.js
@@ -13,17 +13,20 @@ const router = express.Router();
 
 // This section will help you get a list of all the records.
 router.get("/", async (req, res) => {
+  console.log("a")
   let collection = await db.collection("records");
   let results = await collection.find({}).toArray();
+  console.log("asdf")
   res.send(results).status(200);
 });
 
 // This section will help you get a single record by id
 router.get("/:id", async (req, res) => {
+  console.log("b")
   let collection = await db.collection("records");
   let query = { _id: new ObjectId(req.params.id) };
   let result = await collection.findOne(query);
-
+  console.log("bsdf")
   if (!result) res.send("Not found").status(404);
   else res.send(result).status(200);
 });
