1fd03ab055c4142061919c51fbd3f300e265d84b
Zaiqin <phuazaiqin@gmail.com>
2024-05-07 16:55:07 +0800

    revert

--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@ import cors from "cors";
 import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5050;
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5050"; // Change the default value to match your development environment
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(`${API_BASE_URL}/record`, records);
+app.use("/record", records);
 
 // start the Express server
 app.listen(PORT, () => {
