d04fb83bdbcc75921d919db68ab80a1f4e19bdf5
Zaiqin <phuazaiqin@gmail.com>
2024-05-08 22:33:55 +0800

    Used .env

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file
 
 const PORT = process.env.PORT || 5050;
+const SERVER_URI = process.env.SERVER_URI || `http://localhost:${PORT}`; // Define server URI
+
 const app = express();
 
 app.use(cors());
@@ -10,11 +15,12 @@ app.use(express.json());
 app.use("/api/record", records); // Route all API endpoints with /api/records to records router
 
 app.get("/test", async (req, res) => {
-  console.log("testing main")
+  console.log("testing main");
   res.send("testing main here").status(200);
 });
 
 // start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+  console.log(`Server URI: ${SERVER_URI}`);
 });
