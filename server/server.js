cbebac803b470e52349af4884ea90be9503ca3f9
Zaiqin <phuazaiqin@gmail.com>
2024-05-08 14:17:33 +0800

    updates

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 import express from "express";
 import cors from "cors";
-import router from "./routes/record.js";
+import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/record", router); // uses /api/record for all API calls
+app.use("/api/record", records); // Route all API endpoints with /api/records to records router
+
+app.get("/test", async (req, res) => {
+  console.log("testing main")
+  res.send("testing main here").status(200);
+});
 
 // start the Express server
 app.listen(PORT, () => {
