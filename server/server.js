575261cd6639c7dac5341a4b4dffdd50463addbd
Zaiqin <phuazaiqin@gmail.com>
2024-05-09 00:45:59 +0800

    Reverted any cors

--- server/server.js
+++ server/server.js
@@ -5,12 +5,7 @@ import records from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-// Allow requests from your Vercel-hosted frontend
-const corsOptions = {
-  origin: 'https://mern-client-eight-nu.vercel.app'
-};
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/record", records); // Route all API endpoints with /api/records to records router
 
