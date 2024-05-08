176191a07c74671e36c7c88b71ab4e3856584b13
Zaiqin <phuazaiqin@gmail.com>
2024-05-09 01:17:10 +0800

    Added comments

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import records from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+app.use(cors()); // Enable All CORS Requests
 app.use(express.json());
 app.use("/api/record", records); // Route all API endpoints with /api/records to records router
 
