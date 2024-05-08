d6b1882cff54bf6334f02fe08639dd42f5a70f49
Zaiqin <phuazaiqin@gmail.com>
2024-05-09 01:11:23 +0800

    Revert any cors restriction

--- server/server.js
+++ server/server.js
@@ -5,22 +5,13 @@ import records from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-const options = {
-  origin: "http://localhost:5173"
-}
-
-app.use(cors(options));
+app.use(cors());
 app.use(express.json());
 app.use("/api/record", records); // Route all API endpoints with /api/records to records router
 
 app.get("/test", async (req, res) => {
   console.log("testing main")
   res.send("testing main heres").status(200);
-});
-
-app.get("/abc", cors(options), async (req, res) => {
-  console.log("testing cors")
-  res.send("testing cors heres").status(200);
 });
 
 // start the Express server
