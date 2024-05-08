0389670e975e0f1aa77e44cb167f0f71c67a61a0
Zaiqin <phuazaiqin@gmail.com>
2024-05-09 01:08:40 +0800

    try cors

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ import records from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/record", records); // Route all API endpoints with /api/records to records router
-
 const options = {
   origin: "http://localhost:5173"
 }
+
+app.use(cors(options));
+app.use(express.json());
+app.use("/api/record", records); // Route all API endpoints with /api/records to records router
 
 app.get("/test", async (req, res) => {
   console.log("testing main")
